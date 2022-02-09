215b8c088fc6d7884d02fbf97042ba9c0ed4e04d
Seda Unal <seda@Sedas-MacBook-Air.local>
2022-02-09 09:01:15 +0300

    fixed signin link

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -31,7 +31,7 @@ const Header = ({ currentUser, hidden }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="signin">
+        <Link className="option" to="/signin">
           SIGN IN
         </Link>
       )}
